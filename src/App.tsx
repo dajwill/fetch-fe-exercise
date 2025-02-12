1fff16f5a4779ef7f7f055b683691156550c5e2b
dajwill <dajwilliam@gmail.com>
2025-02-12 15:12:14 -0500

    hook up login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
-import './App.css'
 import { Provider } from './components/ui/provider'
-import { Route, Switch } from 'wouter'
+import { Route, Switch, useLocation } from 'wouter'
 import Login from './pages/Login'
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Dogs from './pages/Dogs'
+import { Toaster, toaster } from '@/components/ui/toaster'
+import { useCallback } from 'react'
+import Favorites from './pages/Match'
+import Navbar from './components/Navbar'
+
+const QueryProvider = ({ children }: React.PropsWithChildren) => {
+    const [_, navigate] = useLocation();
+    const toast = useCallback((status?: number) => {
+        if (status === 401) {
+            toaster.error({
+                title: "Unauthorized. Please login.",
+                id: 'auth_error',
+                action: {
+                    label: "Login",
+                    onClick: () => navigate('/'),
+                },
+            })
+        }
+    }, [navigate])
+    const queryClient = new QueryClient({
+        mutationCache: new MutationCache({
+            onError: (error: any) => toast(error.status as number | undefined)
+        }),
+        queryCache: new QueryCache({
+            onError: (error: any) => toast(error.status as number | undefined)
+        })
+    })
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
 
 function App() {
 
-  return (
-    <Provider>
-      <Switch>
-        <Route path="/" component={Login} />
-      </Switch>
-    </Provider>
-  )
+    return (
+        <Provider>
+            <QueryProvider>
+                <Navbar position="sticky" top="0" zIndex="docked" />
+                <Switch>
+                    <Route path="/" component={Login} />
+                    <Route path="/browse" component={Dogs} />
+                    <Route path="/match/:id" component={Favorites} />
+                </Switch>
+            </QueryProvider>
+            <Toaster />
+        </Provider>
+    )
 }
 
 export default App
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,49 @@
-const Login = () => {
-    return (
-        <h1>Login</h1>
-    )
-};
+import { Container, Heading, Input, Stack, Text, Button } from '@chakra-ui/react'
+import { Field } from '@/components/ui/field'
+import useLogin from '@/queries/useLogin'
+import { useCallback } from 'react'
+import { useLocation } from 'wouter'
 
-export default Login;
+
+export const Block = () => {
+    const [_, navigate] = useLocation();
+    const { mutate } = useLogin();
+
+    const onSubmit = useCallback((formData: FormData) => {
+        const name = formData.get('name') as string | null
+        const email = formData.get('email') as string | null
+
+        if (!name || !email) return;
+
+        mutate({name, email}, {
+            onSuccess: () => navigate('/browse')
+        })
+    }, [mutate])
+
+    return (
+        <Container maxW="md" py={{ base: '12', md: '24' }}>
+            <form action={onSubmit}>
+                <Stack gap={{ base: '2', md: '3' }} textAlign="center">
+                    <Heading size={{ base: '2xl', md: '3xl' }}>Welcome</Heading>
+                    <Text color="fg.muted">Login to browse dogs.</Text>
+                </Stack>
+    
+                <Stack gap="6">
+                    <Stack gap="4">
+                        <Field label="Name" required>
+                            <Input type="text" name='name' />
+                        </Field>
+                        <Field label="Email" required>
+                            <Input type="email" name="email" />
+                        </Field>
+                    </Stack>
+                    <Stack>
+                        <Button type="submit">Sign in</Button>
+                    </Stack>
+                </Stack>
+            </form>
+        </Container>
+    )
+}
+
+export default Block;
